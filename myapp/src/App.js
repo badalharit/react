c5edad80e235d259a8b7e8246ded4ec6a01a781f
badalharit <badalharitmrt@gmail.com>
2022-02-07 21:53:53 +0530

    Adding React Codebase With Node Modules

---
+++ myapp/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+
+  /*
+   * Example of an Imperative style of code (JS)
+   */
+  const paragraph = document.createElement("p");
+  paragraph.textContent = "Hello, I'm an example of Imperative style of code (JS).!";
+  document.getElementById('root').append(paragraph);
+
+   /*
+   * Example of an Declarative style of code (React JS) 
+   */
+
+  return (
+    <div>
+      <h3>Hello World</h3>
+      <p>Hello, I'm an example of Declarative style of code (React JS).!</p>
+    </div>
+  );
+}
+
+export default App;
