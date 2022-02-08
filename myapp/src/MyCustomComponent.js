9311a6931095fb491162a6208e25b145cc72c735
badalharit <badalharitmrt@gmail.com>
2022-02-08 21:45:25 +0530

    Using CSS & Multiple elements in a component

--- myapp/src/MyCustomComponent.js
+++ myapp/src/MyCustomComponent.js
@@ -1,8 +1,16 @@
+import "./MyCustomComponent.css";
+
 function MyCustomComponent(){
     return(
-    <h4>
-        Hello Nigga, I'm Fukn Component -_-
-    </h4>
+    <div className="task-item">    
+        <div> 8th June 2022 </div>
+        <div className="task-item__description">
+            <h2>
+                Blockchain Course
+            </h2>
+            <div className="task-item__priority">1</div>
+        </div>
+    </div>
     );
 }
 export default MyCustomComponent;
