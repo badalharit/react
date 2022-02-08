0acc6037a1b9447016e9ad49949cf72350880027
badalharit <badalharitmrt@gmail.com>
2022-02-08 23:09:36 +0530

    Using Props with Dynamic data with function callings

--- myapp/src/MyCustomComponent.js
+++ myapp/src/MyCustomComponent.js
@@ -7,19 +7,18 @@ function MyCustomComponent(props){
      *---------------------------------------------------------------------------------------------
      */
 
-    const todaysDate = new Date();
-    const priority = 1;
-
+    const string = 'Current Date & Time:';
     function getCourseName(){
-        return "Blockchain with React JS"
+        return "Course Name: "
     }
 
     return(
     <div className="task-item">    
+        <div><b>{string}</b></div> &nbsp;
         <div> {props.todaysDate.toISOString()} </div>
         <div className="task-item__description">
             <h2>
-                {props.courseTitle}
+            <i>{getCourseName()}</i>{props.courseTitle}
             </h2>
             <div className="task-item__priority">{props.priority}</div>
         </div>
