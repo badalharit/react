e91d6eb29ece980f647cabfa8c4bd280ac1c45cd
badalharit <badalharitmrt@gmail.com>
2022-02-09 22:02:47 +0530

    `ToDo List` Date Functions & Code Refactoring

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,25 +3,14 @@ import MyCustomComponent from './MyCustomComponent';
 
 function App() {
 
-    /*
-    * Example of Imperative style of code (JS)
-    */
-    // const paragraph = document.createElement("p");
-    // paragraph.textContent = "Hello, I'm an example of Imperative style of code (JS).!";
-    // document.getElementById('root').append(paragraph);
-
-    /*
-    * Example of Declarative style of code (React JS)
-    */
-
     const items = [
       {
-        todaysDate : new Date(),
+        dateValue : new Date(),
         courseTitle: 'Solidity & Etherium',
         priority: 1
       },
       {
-        todaysDate : new Date(),
+        dateValue : new Date(),
         courseTitle: 'Smart Contracts & Etherium',
         priority: 2
       },
@@ -29,13 +18,12 @@ function App() {
 
     return (
       <div>
-        <h3>Hello World</h3>
-        <p>Hello, I'm an example of Declarative style of code (React JS).!</p>
-        <MyCustomComponent todaysDate={items[0].todaysDate}
+        <h1>To-Do List</h1>
+        <MyCustomComponent dateValue={items[0].dateValue}
                            courseTitle={items[0].courseTitle} 
                            priority={items[0].priority}>
         </MyCustomComponent>
-        <MyCustomComponent todaysDate={items[1].todaysDate}
+        <MyCustomComponent dateValue={items[1].dateValue}
                            courseTitle={items[1].courseTitle} 
                            priority={items[1].priority}>
         </MyCustomComponent>
--- myapp/src/MyCustomComponent.js
+++ myapp/src/MyCustomComponent.js
@@ -2,27 +2,23 @@ import "./MyCustomComponent.css";
 
 function MyCustomComponent(props){
 
-    /*---------------------------------------------------------------------------------------------
-     * Dynamic Data & Function Calings
-     *---------------------------------------------------------------------------------------------
-     */
-
-    const string = 'Current Date & Time:';
-    function getCourseName(){
-        return "Course Name: "
-    }
+    const month = props.dateValue.toLocaleString('en-US',{month:'long'});
+    const day = props.dateValue.toLocaleString('en-US',{day:'2-digit'});
+    const year = props.dateValue.getFullYear();
 
     return(
     <div className="task-item">    
-        <div><b>{string}</b></div> &nbsp;
-        <div> {props.todaysDate.toISOString()} </div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
         <div className="task-item__description">
-            <h2>
-            <i>{getCourseName()}</i>{props.courseTitle}
-            </h2>
+            <h2>{props.courseTitle}</h2>
             <div className="task-item__priority">{props.priority}</div>
         </div>
     </div>
     );
+    
 }
 export default MyCustomComponent;
